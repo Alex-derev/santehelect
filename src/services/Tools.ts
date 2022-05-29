ae0e8d00ccc2e7a7cadd34759223c9abcf45e049
Sanya <sasha.derevyanko.1999@mail.ru>
2022-05-29 20:35:07 +0300

    image host

--- src/services/Tools.ts
+++ src/services/Tools.ts
@@ -1,3 +1,3 @@
 export function getImagePath (hash: string): string {
-	return `http://img.dev.santehelect.com/gallery/${hash[0]}/${hash[0]}${hash[1]}/${hash}`;
+	return `${process.env.IMAGE_HOST}/gallery/${hash[0]}/${hash[0]}${hash[1]}/${hash}`;
 }
